6fc2929e0f41204e20f61e539b2fd08c01697974
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-02-14 14:27:53 -0300

    ADD nova classe para manipular as collection cell

---
+++ 100DaysOfSwift-Project9/PersonCell.swift
@@ -0,0 +1,12 @@
+//
+//  PersonCell.swift
+//  100DaysOfSwift-Project9
+//
+//  Created by João Gabriel Dourado Cervo on 14/02/21.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+}
--- 100DaysOfSwift-Project9/ViewController.swift
+++ 100DaysOfSwift-Project9/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ViewController: UICollectionViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
