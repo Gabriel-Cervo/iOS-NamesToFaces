4d2f61c5f75fdc930ee1ada64669572e790c2a9d
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-02-18 09:41:22 -0300

    ADD escolha de imagem e armazenamento no disco

--- 100DaysOfSwift-Project9/ViewController.swift
+++ 100DaysOfSwift-Project9/ViewController.swift
@@ -36,7 +36,22 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     
     // returns when the user selected an image and it's being returned to you.
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        // codigo
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        dismiss(animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        return paths[0]
     }
 
 
